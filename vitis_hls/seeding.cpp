660ceb68b48262304ed6e8125f908a8fd7ede4ae
Julien Eudine <julien@eudine.fr>
2021-10-13 11:45:27 +0200

    Vitis HLS interface defined

---
+++ vitis_hls/seeding.cpp
@@ -0,0 +1,16 @@
+#include "seeding.h"
+#define W 12
+#define K 18
+#define MIN_T 3
+#define LOC_R 200
+#define B 26
+#define LEN_READ 100
+
+void seeding(const uint32_t h[H_SIZE], const uint32_t location[LS_SIZE], const ap_uint<1> strand[LS_SIZE], const base_t *read, uint32_t *dst) {
+#pragma HLS INTERFACE mode=ap_memory port=h bundle=gmem0
+#pragma HLS INTERFACE mode=ap_memory port=location bundle=gmem0
+#pragma HLS INTERFACE mode=ap_memory port=strand bundle=gmem1
+
+#pragma HLS INTERFACE m_axi port=read depth=100 //LEN_READ
+#pragma HLS INTERFACE m_axi port=dst depth=100 //LEN_READ
+}
