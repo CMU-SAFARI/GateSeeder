e5f78e6a200dc4e95d0cbd96b5be50e443507533
Julien Eudine <julien@eudine.fr>
2021-10-24 11:58:39 +0200

    Testbench wip

--- fpga_impl/src/tb_top.cpp
+++ fpga_impl/src/tb_top.cpp
@@ -1,3 +1,18 @@
 #include "tb_driver.hpp"
+#include <fstream>
+#include <stdio.h>
+#include <vector>
 
-int main() { return 0; }
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Error: Wrong number of arguments\n");
+        exit(2);
+    }
+    ifstream ifs_read, ifs_idx;
+    ifs_read.open(argv[1], ifstream::in);
+    ifs_idx.open(argv[2], ifstream::in | ifstream::binary);
+    vector<out_loc_t> res = drive_sim(ifs_read, ifs_idx);
+    return 0;
+}
