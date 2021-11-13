a51c435609c94f3263cf6d1989c6110eb9eb5389
Julien Eudine <julien@eudine.fr>
2021-11-13 17:24:56 +0100

    Multithread formated

--- cpu_impl/src/main.c
+++ cpu_impl/src/main.c
@@ -2,6 +2,7 @@
 #include "seeding.h"
 #include "unistd.h"
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 
 int main(int argc, char *argv[]) {
@@ -10,6 +11,7 @@ int main(int argc, char *argv[]) {
 	FILE *fp_idx;
 	char idx_flag = 0;
 	char o_flag   = 0;
+	char fp_name[200];
 #ifdef MULTI_THREAD
 	FILE *fp_o[NB_THREADS];
 #else
@@ -23,17 +25,18 @@ int main(int argc, char *argv[]) {
 					return 1;
 				}
 				parse_index(fp_idx, &idx);
-				fprintf(stderr, "index `%s` parsed\n", optarg);
+				printf("Info: index `%s` parsed\n", optarg);
 				idx_flag = 1;
 				break;
 			case 'o':
 #ifdef MULTI_THREAD
 				for (unsigned i = 0; i < NB_THREADS; i++) {
 					char name_buf[200];
-					sprintf(name_buf, "%s_%u.dat", optarg, i);
+					strcpy(fp_name, optarg);
+					sprintf(name_buf, "%s_%u.dat", fp_name, i);
 					fp_o[i] = fopen(name_buf, "w");
 					if (fp_o[i] == NULL) {
-						fprintf(stderr, "Error: cannot open `%s`\n", optarg);
+						fprintf(stderr, "Error: cannot open `%s`\n", fp_name);
 						return 1;
 					}
 				}
@@ -62,19 +65,21 @@ int main(int argc, char *argv[]) {
 	}
 	read_v reads;
 	parse_fastq(fp_fastq, &reads);
-	fprintf(stderr, "reads `%s` parsed\n", argv[optind]);
-	fputs("\t SEEDING STARTS\n", stderr);
+	printf("Info: reads `%s` parsed\n", argv[optind]);
+	puts("\t SEEDING STARTS");
 	struct timespec start, finish;
 	clock_gettime(CLOCK_MONOTONIC, &start);
-
-#ifdef MULTI_THREAD
 	read_seeding(idx, reads, fp_o);
+	clock_gettime(CLOCK_MONOTONIC, &finish);
+#ifdef MULTI_THREAD
+	for (unsigned i = 0; i < NB_THREADS; i++) {
+		printf("\t * %s_%u.dat written\n", fp_name, i);
+	}
 #else
 #endif
 
 	clock_gettime(CLOCK_MONOTONIC, &finish);
-	fputs("\t SEEDING IS OVER\n", stderr);
-	fprintf(stderr, "Time: %f sec\n",
-	        finish.tv_sec - start.tv_sec + (finish.tv_nsec - start.tv_nsec) / 1000000000.0);
+	puts("\t SEEDING FINISHED");
+	printf("Time: %f sec\n", finish.tv_sec - start.tv_sec + (finish.tv_nsec - start.tv_nsec) / 1000000000.0);
 	return 0;
 }
