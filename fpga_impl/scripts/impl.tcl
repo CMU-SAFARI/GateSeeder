a7f56343a0ccc1686bc6ef81cf2e6f563926935f
julien <julien@eudine.fr>
2022-12-12 10:26:04 +0100

    unused file removed

--- fpga_impl/scripts/impl.tcl
+++
@@ -1,4 +0,0 @@
-source scripts/vitis_hls.tcl
-csynth_design
-#export_design -format xo -output seeding.xo
-exit
--- fpga_impl/src/hw/extraction.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef EXTRACTION_HPP
-#define EXTRACTION_HPP
-#include "kernel.hpp"
-void extract_seeds(hls::stream<base_t> &seq_i, hls::stream<seed_t> &p0_o, hls::stream<seed_t> &p1_o);
-void push_seed(seed_t *p, hls::stream<seed_t> &p0_o, hls::stream<seed_t> &p1_o, ap_uint<SEED_BUF_LEN_LOG> &p_l,
-               hash_t val);
-#endif
