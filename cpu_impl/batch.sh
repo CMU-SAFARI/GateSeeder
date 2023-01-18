7619180cc3d9023438e201f16aa7949ab576c6d1
julien <julien@eudine.fr>
2023-01-18 16:11:46 +0100

    distribution plotted

--- cpu_impl/batch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in {1..60}
-do
-	srun make NB_THREADS=$i run
-	make clean
-done
-exit 0
--- gold/src/main.c
+++ gold/src/main.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]) {
 		errx(1, "Usage\t%s <index.dti> <query.fastq>", argv[0]);
 	}
 
-	index_t index = parse_index(argv[1]);
+	index_t index = index_parse(argv[1]);
 	int fd        = open(argv[2], O_RDONLY);
 	if (fd == -1) {
 		err(1, "open %s", argv[2]);
@@ -47,7 +47,13 @@ int main(int argc, char *argv[]) {
 	while (input.nb_reads != 0) {
 		for (unsigned i = 0; i < input.nb_reads; i++) {
 			extract_seeds_mapping(input.reads[i].seq, input.reads[i].len, &minimizers, 0);
-#ifdef DEBUG_SEED_EXTRACTION
+#ifdef HASH_DISTRIBUTION
+			for (uint32_t j = 0; j < minimizers.len - 1; j++) {
+				printf("%ld\n",
+				       (int64_t)minimizers.seeds[j].hash - (int64_t)minimizers.seeds[j + 1].hash);
+			}
+#endif
+#ifdef SEED_EXTRACTION
 			for (uint32_t j = 0; j < minimizers.len; j++) {
 				printf("hash: %010lx, loc: %x\n", minimizers.seeds[j].hash, minimizers.seeds[j].loc);
 			}
@@ -55,7 +61,7 @@ int main(int argc, char *argv[]) {
 #endif
 			query_index_map(minimizers, index.map, &key_pos);
 
-#ifdef DEBUG_QUERY_INDEX_MAP
+#ifdef QUERY_INDEX_MAP
 			for (uint32_t j = 0; j < key_pos.len; j++) {
 				printf("start_pos: %x, end_pos: %x, seed_id: %x, loc: %x\n", key_pos.poss[j].start_pos,
 				       key_pos.poss[j].end_pos, key_pos.poss[j].seed_id, key_pos.poss[j].query_loc);
@@ -63,12 +69,13 @@ int main(int argc, char *argv[]) {
 			puts("");
 #endif
 			query_index_key(key_pos, index.key, &loc);
+#ifdef QUERY_INDEX_KEY
 			printf("loc_len: %u\n", loc.len);
 			for (uint32_t j = 0; j < loc.len; j++) {
 				printf("target_loc: %x, query_loc: %x, chrom_id: %x, str: %x\n", loc.locs[j].target_loc,
 				       loc.locs[j].query_loc, loc.locs[j].chrom_id, loc.locs[j].str);
 			}
-			free(loc.locs);
+#endif
 		}
 		parse_reads(&input);
 	}
