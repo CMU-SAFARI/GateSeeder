97c1a7f35a3f6aa70150d2004cb2bb1bf67c8e27
julien <julien@eudine.fr>
2023-02-09 14:39:22 +0100

    accuracy done

--- evaluation/scripts/accuracy.sh
+++ evaluation/scripts/accuracy.sh
@@ -1,58 +1,70 @@
 #!/bin/bash
-TARGET=$DATA/GCA_000001405.15_GRCh38_no_alt_analysis_set.fasta
-QUERY=sim.fasta
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <ont|hifi|illumina>"
+    exit 1
+fi
+
+query=$DATA/$1_sim.fasta
 
 if [ $1 = 'ont' ]; then
-	W=10
-	K=15
-	RANGE_MAX_OCC="10 50 100"
-	RANGE_VT_DISTANCE=$(eval echo "{500..1100..50}")
-	MM2_PRESET='map-ont'
-	nb_reads=$(expr $(wc -l < $QUERY) / 2)
+	w=10
+	k=15
+	range_max_occ="10 50 100"
+	range_vt_distance=$(eval echo "{500..1000..50}")
+	extra_param=""
+	minimap2_preset='map-ont'
+	nb_reads=$(expr $(wc -l < $query) / 2)
 elif [ $1 = 'hifi' ]; then
-	W=19
-	K=19
-	RANGE_MAX_OCC="1 2 5"
-	RANGE_VT_DISTANCE=$(eval echo "{500..5000..250}")
-	MM2_PRESET='map-hifi'
-	nb_reads=$(expr $(wc -l < $QUERY) / 2)
+	w=19
+	k=19
+	range_max_occ="1 2 5"
+	range_vt_distance=$(eval echo "{1000..5000..500}")
+	extra_param=""
+	minimap2_preset='map-hifi'
+	nb_reads=$(expr $(wc -l < $query) / 2)
 elif [ $1 = 'illumina' ]; then
-	W=11
-	K=21
-	RANGE_MAX_OCC="500"
-	RANGE_VT_DISTANCE=$(eval echo "{10..150..20}")
-	MM2_PRESET='sr'
-	nb_reads=$(expr $(wc -l < $QUERY) / 4)
+	w=11
+	k=21
+	range_max_occ="20 60 180"
+	range_vt_distance="10 30 50 100 150"
+	extra_param="-e -c 0"
+	minimap2_preset='sr'
+	nb_reads=$(expr $(wc -l < $query) / 4)
 else
+    echo "Usage: $0 <ont|hifi|illumina>"
 	exit 1
 fi
 
-XCLBIN=../device/demeter_$1.xclbin
-RES_DIR=accuracy_$1
-mkdir -p $RES_DIR
-rm -f $RES_DIR/*
+target=$DATA/GCA_000001405.15_GRCh38_no_alt_analysis_set.fasta
+paf=$DATA/mapping.paf
 
-echo -e "$1\t$nb_reads" > $RES_DIR/info.dat
+xclbin=../device/demeter_$1.xclbin
+res_dir=accuracy_$1
+mkdir -p $res_dir
+rm -f $res_dir/*
 
-for max_occ in $RANGE_MAX_OCC
+echo -e "$1\t$nb_reads" > $res_dir/info.dat
+
+for max_occ in $range_max_occ
 do
 	echo "[ACC] Generating the index with max_occ: $max_occ"
-	../demeter_index -t 32 -w $W -k $K -f $max_occ $TARGET $DATA/index.gfi
-	for vt_distance in $RANGE_VT_DISTANCE
+	../seedfarm_index -t 32 -w $w -k $k -f $max_occ $target $DATA/index.sfi
+	for vt_distance in $range_vt_distance
 	do
-		echo "[ACC] Running demeter with vt_distance: $vt_distance"
-		../demeter -t 32 -d $vt_distance $XCLBIN $DATA/index.gfi $QUERY -o mapping.paf
+		echo "[ACC] Running seedfarm with vt_distance: $vt_distance"
+		../seedfarm -t 32 -d $vt_distance $extra_param $xclbin $DATA/index.sfi $query -o $paf
 		echo "[ACC] Evaluating the results"
-		echo -e "max_occ: $max_occ, vt_dst: $vt_distance" > $RES_DIR/seedfarm_${max_occ}_${vt_distance}_$1.dat
-		paftools.js mapeval mapping.paf >> $RES_DIR/seedfarm_${max_occ}_${vt_distance}_$1.dat
+		echo -e "max_occ: $max_occ, vt_dst: $vt_distance" > $res_dir/seedfarm_${max_occ}_${vt_distance}_$1.dat
+		paftools.js mapeval $paf >> $res_dir/seedfarm_${max_occ}_${vt_distance}_$1.dat
 	done
 done
 
 echo "[ACC] Running minimap2"
-minimap2 -t 32 -x $MM2_PRESET -o mapping.paf $TARGET $QUERY
+minimap2 -t 32 -x $minimap2_preset -o $paf $target $query
 echo "[ACC] Evaluating the results"
-echo -e "minimap2" > $RES_DIR/minimap2_$1.dat
-paftools.js mapeval mapping.paf >> $RES_DIR/minimap2_$1.dat
+echo -e "minimap2" > $res_dir/minimap2_$1.dat
+paftools.js mapeval $paf >> $res_dir/minimap2_$1.dat
 
-rm -f $DATA/index.gfi
-rm -f mapping.paf
+rm -f $DATA/index.sfi
+rm -f $paf
